30045bf05f54d1ebf0497fd588e0fce5aa365d23
Julius Caezar Coros <72971299+juliusdayo@users.noreply.github.com>
2022-03-22 20:35:12 +0800

    update start, fixed models

--- models/comments.js
+++ models/comments.js
@@ -3,10 +3,10 @@ const mongoose = require("mongoose");
 const { Schema } = mongoose;
 
 const commentSchema = new Schema({
-    author:Number,
-    datePosted:new Date(),
-    topic: String,
-    content:Number,
+  author: Number,
+  datePosted: Date,
+  topic: String,
+  content: Number,
 });
 
 const commentModel = mongoose.model("Comments", commentSchema);
--- models/posts.js
+++ models/posts.js
@@ -3,11 +3,11 @@ const mongoose = require("mongoose");
 const { Schema } = mongoose;
 
 const postSchema = new Schema({
-    author:Number,
-    comments: [{ type: Schema.Types.ObjectId, ref: 'Comments' }],
-    datePosted:new Date(),
-    topic: String,
-    content:Number,
+  author: Number,
+  comments: [{ type: Schema.Types.ObjectId, ref: "Comments" }],
+  datePosted: Date,
+  topic: String,
+  content: Number,
 });
 
 const postModel = mongoose.model("Posts", postSchema);
