cdf9296c11a092bd65ad27115b2b1abdf21df93a
Julius Caezar Coros <72971299+juliusdayo@users.noreply.github.com>
2022-03-24 08:41:30 +0800

    Update comments.js

--- models/comments.js
+++ models/comments.js
@@ -3,10 +3,11 @@ const mongoose = require("mongoose");
 const { Schema } = mongoose;
 
 const commentSchema = new Schema({
-  author: Number,
-  datePosted: Date,
-  topic: String,
-  content: Number,
+  author: String,
+  datePosted: { type: Date, default: Date.now },
+
+  content: String,
+  post: { type: Schema.Types.ObjectId, ref: "Post" },
 });
 
 const commentModel = mongoose.model("Comments", commentSchema);
